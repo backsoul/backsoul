8f7f7547febf77aa0dab672e12a3a9b8438aa91e
DanielSarmientoDev <danielsarmientodeveloper@gmail.com>
2021-08-21 10:02:47 -0500

    rename docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Backsoul',
@@ -17,7 +17,7 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { href: "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap", rel: "stylesheet" }
-      
+
     ],
   },
 
@@ -47,4 +47,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  router: {
+    base: '/backsoul/'
+  }
 }
